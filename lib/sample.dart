a2dc36588fa6a96c1eda07e2cc9e9da294ddc78a
Amarnath A <a.amarnath@hotmail.com>
2025-02-07 08:29:35 -0600

    Update sample.dart

--- lib/sample.dart
+++ lib/sample.dart
@@ -1,52 +1,16 @@
-import 'package:oz_core/oz_core.dart';
-import 'package:oz_smart_key/src/core/services/hid/hid_service_monitored.dart';
-import 'package:zig_flutter_hid/models/pk_pass.dart';
-import 'package:zig_flutter_hid/services/zig_flutter_hid_platform_interface.dart';
+package com.example.yourapp
 
-part 'view_in_wallet_state.dart';
+import android.content.Context
+import android.view.View
+import com.google.android.gms.wallet.button.AddToGoogleWalletButton
+import io.flutter.plugin.platform.PlatformView
 
-class ViewInWalletCubit extends Cubit<ViewInWalletState> {
-  final ZIGFlutterHidPlatform zIGFlutterHidPlatform;
-  final HidServiceMonitored hidService;
+class GoogleWalletButtonView(context: Context) : PlatformView {
+    private val walletButton: AddToGoogleWalletButton = AddToGoogleWalletButton(context)
 
-  
-  ViewInWalletCubit({required this.zIGFlutterHidPlatform, required this.hidService})
-      : super(const ViewInWalletState());
-      
- bool isPassAvailable=false;
- bool isRemotePassAvailable =false;
- bool showViewWallet = false;
-
-  Future<void> checkPassesAvailability() async {
-    emit(state.copyWith(status: ViewInWalletStatus.loading));
-    try {
-    await hidService.startHid();
-    isPassAvailable = await zIGFlutterHidPlatform.isDevicePassAvailable();
-    isRemotePassAvailable = await zIGFlutterHidPlatform.isRemotePassAvailable();
-    showViewWallet = isPassAvailable || (isPassAvailable && isRemotePassAvailable);
-    print('AMAR isPassAvailable :$isPassAvailable');
-     print('AMAR isRemotePassAvailable :$isRemotePassAvailable');
-     print('AMAR showViewWallet1 :$showViewWallet');
-     print('AMAR showViewWallet 2:$showViewWallet');
-    emit(state.copyWith(
-        status: ViewInWalletStatus.success, 
-       isPassAvailable: isPassAvailable,
-       isRemotePassAvailable: isRemotePassAvailable,
-       showViewWallet:showViewWallet,
-    ));
-    } catch (e) {
-      emit(state.copyWith(status: ViewInWalletStatus.error));
+    override fun getView(): View {
+        return walletButton
     }
-  }
 
-  Future<void> showPass() async {
-     emit(state.copyWith(status: ViewInWalletStatus.loading));
-     await zIGFlutterHidPlatform.showPassInWallet();
-     emit(state.copyWith(
-      status: ViewInWalletStatus.success,
-     isPassAvailable: isPassAvailable,
-     isRemotePassAvailable: isRemotePassAvailable,
-     showViewWallet:showViewWallet,
-    ));
-  }
+    override fun dispose() {}
 }

8ab322f04ef3d2ac1bc969af5ffcd9e980cf87aa
Amarnath A <a.amarnath@hotmail.com>
2025-02-07 14:07:23 -0600

    Update sample.dart

--- lib/sample.dart
+++ lib/sample.dart
@@ -1,3 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Add to Google Wallet')),
+        body: Center(
+          child: AddToGoogleWalletButton(
+            onPressed: () {
+              // Handle button press
+              print('Add to Google Wallet button pressed');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AddToGoogleWalletButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
   const AddToGoogleWalletButton({required this.onPressed});
 
   @override

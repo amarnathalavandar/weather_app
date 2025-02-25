6ac7daf8dff17855dcade722bf69c0baf9095d42
Amarnath A <a.amarnath@hotmail.com>
2025-02-25 11:58:18 -0600

    Update test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -2,36 +2,37 @@ import 'package:credentialtool_web/config/themes/theme.dart';
 import 'package:credentialtool_web/data/datasources/remote/api/hid_origo_api.dart';
 import 'package:credentialtool_web/data/datasources/zct_hid_origo_datasource.dart';
 import 'package:credentialtool_web/domain/repositories/user_management_repository.dart';
-import 'package:dio/src/dio.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'presentation/pages/user_management/user_management_screen.dart';
 
 void main() {
-  runApp(MyApp(
+  runApp(
     MultiRepositoryProvider(
       providers: [
         RepositoryProvider<ZctHidIOrigoApi>(
           create: (context) => ZctHidIOrigoApi(Dio()),
         ),
         RepositoryProvider<OZUserRemoteDataSourceImpl>(
-          create: (context) => OZUserRemoteDataSourceImpl(api: RepositoryProvider.of<ZctHidIOrigoApi>(context)),
+          create: (context) => OZUserRemoteDataSourceImpl(
+            api: RepositoryProvider.of<ZctHidIOrigoApi>(context),
+          ),
         ),
-        RepositoryProvider(
-          create: (context) => UserManagementRepository(remoteDataSource: RepositoryProvider.of<OZUserRemoteDataSourceImpl>(context)),
-        )
-       
+        RepositoryProvider<UserManagementRepository>(
+          create: (context) => UserManagementRepository(
+            remoteDataSource: RepositoryProvider.of<OZUserRemoteDataSourceImpl>(context),
+          ),
+        ),
       ],
-      child:  MyApp(),
-    )
-
-  ));
+      child: const MyApp(), // Now only provide MyApp once
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,7 +40,7 @@ class MyApp extends StatelessWidget {
       debugShowMaterialGrid: false,
       title: 'Flutter Demo',
       theme: ZCTTheme.standardTheme,
-      home:  UserManagementScreen(),
+      home: const UserManagementScreen(), // Ensure this is wrapped with const if possible
     );
   }
 }
@@ -47,6 +48,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -71,13 +73,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text('You have pushed the button this many times:'),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
@@ -85,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }

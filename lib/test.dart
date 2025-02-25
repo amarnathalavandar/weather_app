bec148e4c4d78d83e93f2c74b88c7adcba453c8e
Amarnath A <a.amarnath@hotmail.com>
2025-02-25 11:57:41 -0600

    Update test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -1,55 +1,91 @@
-
+import 'package:credentialtool_web/config/themes/theme.dart';
 import 'package:credentialtool_web/data/datasources/remote/api/hid_origo_api.dart';
-import 'package:credentialtool_web/domain/models/user_model.dart';
-import 'package:credentialtool_web/utils/constants/models/api_response.g.dart';
+import 'package:credentialtool_web/data/datasources/zct_hid_origo_datasource.dart';
+import 'package:credentialtool_web/domain/repositories/user_management_repository.dart';
+import 'package:dio/src/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'presentation/pages/user_management/user_management_screen.dart';
 
-abstract class OZUserRemoteDataSource {
+void main() {
+  runApp(MyApp(
+    MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<ZctHidIOrigoApi>(
+          create: (context) => ZctHidIOrigoApi(Dio()),
+        ),
+        RepositoryProvider<OZUserRemoteDataSourceImpl>(
+          create: (context) => OZUserRemoteDataSourceImpl(api: RepositoryProvider.of<ZctHidIOrigoApi>(context)),
+        ),
+        RepositoryProvider(
+          create: (context) => UserManagementRepository(remoteDataSource: RepositoryProvider.of<OZUserRemoteDataSourceImpl>(context)),
+        )
+       
+      ],
+      child:  MyApp(),
+    )
 
-
-  Future<User> getUserDetails(String email);
-
-  Future<bool> suspendPass();
-
-  Future<bool> resumePass();
-
-  Future<bool> revokePass();
-
-
+  ));
 }
 
-class OZUserRemoteDataSourceImpl implements OZUserRemoteDataSource {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-
-  final ZctHidIOrigoApi api;
-
-  OZUserRemoteDataSourceImpl({required this.api});
-
-
+  // This widget is the root of your application.
   @override
-  Future<User> getUserDetails(String email) async {
-
-    final response = await api.getFetchUserPassDetails();
-    return response.data.toEmployeeEntity();
-    // TODO: implement getUserDetails
-  }
-  
-  @override
-  Future<bool> resumePass() {
-    // TODO: implement resumePass
-    throw UnimplementedError();
-  }
-  
-  @override
-  Future<bool> revokePass() {
-    // TODO: implement revokePass
-    throw UnimplementedError();
-  }
-  
-  @override
-  Future<bool> suspendPass() {
-    // TODO: implement suspendPass
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      title: 'Flutter Demo',
+      theme: ZCTTheme.standardTheme,
+      home:  UserManagementScreen(),
+    );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}

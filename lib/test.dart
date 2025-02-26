76b9fe6706427306a87081770f6492f94f8f2414
Amarnath A <a.amarnath@hotmail.com>
2025-02-25 21:16:52 -0600

    Update test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -1,1 +1,47 @@
-AMAR  onResponse :reponse :{error: null, timestamp: null, data: {passes: [{id: f18870f7-ee48-4ccb-80cc-456a700f2d00, status: ACTIVE, deviceType: PHONE, userName: AMARNATH, emailAddress: amarnath.alavandar@zurichna.com}]}}
+import 'package:json_annotation/json_annotation.dart';
+import 'user_details_remote_entity.dart';
+
+part 'api_response.g.dart';
+
+@JsonSerializable(genericArgumentFactories: true)
+class ZCTApiResponse<T> {
+  final T? data;
+  final ApiResponseError? error;
+  final String? timestamp;
+
+  ZCTApiResponse({this.data, this.error, this.timestamp});
+
+  factory ZCTApiResponse.fromJson(
+          Map<String, dynamic> json, T Function(Object?) fromJsonT) =>
+      _$ZCTApiResponseFromJson(json, fromJsonT);
+}
+
+@JsonSerializable()
+class PassesResponse {
+  final List<UserDetailsRemoteEntity> passes;
+
+  PassesResponse({required this.passes});
+
+  factory PassesResponse.fromJson(Map<String, dynamic> json) =>
+      _$PassesResponseFromJson(json);
+}
+
+@JsonSerializable()
+class ApiResponseError {
+  final String? message;
+  final String? stackTrace;
+
+  ApiResponseError({this.message, this.stackTrace});
+
+  factory ApiResponseError.fromJson(Map<String, dynamic> json) =>
+      _$ApiResponseErrorFromJson(json);
+}
+
+
+
+
+
+@POST("/origohid/v1.0.0/HIDOrigo/FetchUserPassDetails")
+Future<ZCTApiResponse<PassesResponse>> getFetchUserPassDetails(
+  @Body() Map<String, dynamic> body,
+);

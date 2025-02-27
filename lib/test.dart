52fc8ad1117899fa19173474bf5c1ad78b535d84
Amarnath A <a.amarnath@hotmail.com>
2025-02-26 21:36:18 -0600

    Update test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -1,40 +1,4 @@
-import 'package:flutter/material.dart';
-
-class CustomSnackBar {
-  static void show(BuildContext context, String message) {
-    final snackBar = SnackBar(
-      behavior: SnackBarBehavior.floating,
-      margin: EdgeInsets.only(
-        bottom: MediaQuery.of(context).size.height - kToolbarHeight - 80, // 30px below AppBar
-        left: 20,
-        right: 20,
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      backgroundColor: Colors.red, // Set background color
-      content: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Icon(Icons.error_outline, color: Colors.white),
-          Expanded(
-            child: Text(
-              message,
-              style: TextStyle(color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              ScaffoldMessenger.of(context).hideCurrentSnackBar();
-            },
-            child: Icon(Icons.close, color: Colors.white),
-          ),
-        ],
-      ),
-      duration: Duration(seconds: 2), // Auto dismiss after 2 seconds
-    );
-
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-  }
-}
+   onPressed: () {
+                                            context.read<UserManagementCubit>().updateUserStatus(user.passId, user.passStatus,user.deviceType);
+                                            //CustomSnackBar.show(context, 'The pass was successfully suspended.');
+                                          },

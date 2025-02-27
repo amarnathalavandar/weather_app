e7f667f03543303c049e28bd9717ed1917ee2a09
Amarnath A <a.amarnath@hotmail.com>
2025-02-26 21:22:17 -0600

    Update test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -1,59 +1,67 @@
-import 'package:credentialtool_web/domain/repositories/user_management_repository.dart';
-import 'package:credentialtool_web/presentation/cubit/user_management/cubit/user_management_state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 
-class UserManagementCubit extends Cubit<UserManagementState> {
-  UserManagementCubit(this.userManagementRepository) : super(const UserManagementState());
+void main() {
+  runApp(MyApp());
+}
 
-  final UserManagementRepository userManagementRepository;
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SnackBarDemo(),
+    );
+  }
+}
 
-  // Search for user by email
-  Future<void> searchUser(String query) async {
-    print('Query: $query');
+class SnackBarDemo extends StatelessWidget {
+  void _showSnackBar(BuildContext context) {
+    var snackBar = SnackBar(
+      behavior: SnackBarBehavior.floating,
+      margin: EdgeInsets.only(
+        bottom: MediaQuery.of(context).size.height - kToolbarHeight - 80, // 30px below AppBar
+        left: 20,
+        right: 20,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      backgroundColor: Colors.red, // Set background color
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.error_outline, color: Colors.white),
+          Expanded(
+            child: Text(
+              "Error occurred, please try again later",
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            },
+            child: Icon(Icons.close, color: Colors.white),
+          ),
+        ],
+      ),
+      duration: Duration(seconds: 2), // Auto dismiss after 2 seconds
+    );
 
-    emit(state.copyWith(status: UserStatus.loading));
-
-    final userDetailsList = await userManagementRepository.getUserDetailsByEmailId(query);
-
-    emit(state.copyWith(status: UserStatus.userloaded, userList: userDetailsList));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
-  // Update user status (Suspend/Resume)
-  Future<void> updateUserStatus(String passId, String currentStatus, String deviceType) async {
-    // Set loading state for status change
-    emit(state.copyWith(isStatusLoading: true));
-
-    String newStatus = currentStatus == 'ACTIVE' ? 'SUSPEND' : 'RESUME';
-    print('pass id: $passId - deviceType :$deviceType CURRENT: $currentStatus - NEW :$newStatus');
-
-    // Call the repository to toggle the user status
-    final isStatusChanged = await userManagementRepository.togglePassStatus(passId, newStatus);
-    print("YES STATUS CHANGED: $isStatusChanged");
-
-    if (isStatusChanged) {
-      // Update the user status in the user list
-      final updatedUserList = List.from(state.userList!); // Make a copy of the user list
-      final userIndex = updatedUserList.indexWhere((user) => user.passId == passId);
-
-      if (userIndex != -1) {
-        // If the user is found, update their status
-        updatedUserList[userIndex] = updatedUserList[userIndex].copyWith(
-          passStatus: newStatus,
-        );
-      }
-
-      // Emit the updated state with the new user list
-      emit(state.copyWith(
-        isStatusLoading: false,
-        userList: updatedUserList, // Set the updated list
-        status: UserStatus.userloaded, // Assuming the data was successfully updated
-      ));
-    } else {
-      // Handle failure to change status (e.g., show error)
-      emit(state.copyWith(
-        isStatusLoading: false,
-        errorMessage: 'Failed to update user status.',
-      ));
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Floating SnackBar Example')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _showSnackBar(context),
+          child: Text('Show SnackBar'),
+        ),
+      ),
+    );
   }
 }

71fb893f03856d110c1f05e856bc92031f330433
Amarnath A <a.amarnath@hotmail.com>
2025-03-21 09:49:46 -0500

    Update tes1.dart

--- lib/tes1.dart
+++ lib/tes1.dart
@@ -1,80 +1,24 @@
+import PassKit
+import Flutter
 
-
-import 'package:credentialtool_web/domain/models/user_details.dart';
-import 'package:equatable/equatable.dart';
-
-
-
-enum UserStatus {
-  initial,
-  loading,
-  passesavailable,
-  passesunavailable,
-  /* resume,
-  suspend,
-  revoke, */
-  error;
-
-  bool get isLoading => this == UserStatus.loading;
-  bool get isUserPassesLoaded => this == UserStatus.passesavailable;
-  bool get isUserPassesNotAvailable => this == UserStatus.passesunavailable;
-
-  /* bool get isPassResumed => this == UserStatus.resume;
-  bool get isPassSuspended=> this == UserStatus.suspend;
-  bool get isPassRevoked => this == UserStatus.revoke; */
-  bool get isError => this == UserStatus.error;
+private func showPassInWallet(result: FlutterResult) {
+    let passLibrary = PKPassLibrary()
+    
+    // Check if you have a keysManager and that listWalletPasses is non-empty
+    if let keysManager = keysManager {
+        for pass: PKPass in keysManager.listWalletPasses() as? [PKPass] ?? [] {
+            if let passToPresent = pass.secureElementPass {
+                // Present the pass using PKAddPassesViewController
+                if let addPassVC = PKAddPassesViewController(pass: passToPresent) {
+                    if let viewController = UIApplication.shared.keyWindow?.rootViewController {
+                        viewController.present(addPassVC, animated: true, completion: nil)
+                    }
+                    result(true)  // Return success to Flutter
+                    return
+                }
+            }
+        }
+    }
+    
+    result(false)  // Return failure if no pass is found
 }
-
-class UserManagementState extends Equatable {
-
-  final UserStatus status;
-  final String? errorMessage;
-  final List<UserDetailsEntity>? userList;
-  final bool userStatusUpdated;
-  final String? newPassStatus;
-  final bool isPassRevoked;
-  final String? actionId;
-
-  
-  const UserManagementState({
-    this.status = UserStatus.initial,
-    this.userList, 
-    this.errorMessage,
-    this.userStatusUpdated=false,
-    this.newPassStatus,
-    this.isPassRevoked=false,
-    this.actionId
-    });
-
-
-  @override
-  List<Object?> get props => [
-        status,
-        userList, 
-        errorMessage,
-        actionId
-      ];
-
-UserManagementState copyWith({
-  UserStatus? status,
-  String? errorMessage,
-  List<UserDetailsEntity>? userList, 
-  String? newPassStatus,
-  bool? userStatusUpdated,
-  bool? isPassRevoked,
-  String? actionId
-
-}) {
-  return UserManagementState(
-    userList: userList ?? this.userList,
-    status: status ?? this.status,
-    errorMessage: errorMessage ?? this.errorMessage, 
-    userStatusUpdated:userStatusUpdated?? this.userStatusUpdated,
-    newPassStatus:newPassStatus?? this.newPassStatus,
-    isPassRevoked:isPassRevoked?? this.isPassRevoked,
-    actionId:actionId,
-  );
-}
-
-}
-
